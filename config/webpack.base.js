93fa72edd1024818e858c7ad573d3a0f0dd87651
Ben-Huo <641838789@qq.com>
2019-10-23 12:52:36 +0800

    基础webpack脚手架

---
+++ config/webpack.base.js
@@ -0,0 +1,79 @@
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+module.exports = {
+  // + 提取公共模块配置
+  optimization: {
+    splitChunks: {
+      chunks: "all" // 提取所有文件的共同模块
+    }
+  },
+  // 入口
+  // 用对象的方式配置多个入口
+  entry: {
+    index:'./src/index.js'
+  },
+
+  // 输出
+  output: {
+    // 修改输出路径和文件名，[name]是动态的，读取entry的属性
+    filename: "js/[name].bundle.js",
+    path: path.resolve(__dirname, "../dist") // 注意此处输出目录是父级文件夹
+  },
+
+  // 模块加载器
+  module: {
+    rules: [
+      {
+        // vue
+        test: /\.vue$/,
+        loader: "vue-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "vue-style-loader",
+          use: ["css-loader"]
+        })
+      },
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "vue-style-loader",
+          use: ["css-loader", "less-loader"]
+        })
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              publicPath: "./images/",
+              outputPath: "images"
+            }
+          }
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    // 提取css样式到单独文件
+    new ExtractTextPlugin("style/style2.css"),
+
+    // 每次构建前清除dist目录
+    new CleanWebpackPlugin(),
+
+    // 自动生成index.html到dist
+    new HtmlWebpackPlugin({
+      template: "public/index.html"
+    }),
+    
+    // 插入vue插件
+    new VueLoaderPlugin()
+  ]
+};
---
+++ config/webpack.dev.js
@@ -0,0 +1,14 @@
+const merge = require('webpack-merge');
+const base = require('./webpack.base.js');
+
+module.exports = merge(base, {
+    mode: "development",
+    
+    // 生成map映射文件
+    devtool: "source-map",
+	
+    // 开发服务器配置
+    devServer: {
+        port: 8080 // 默认端口是8080
+   },
+})
